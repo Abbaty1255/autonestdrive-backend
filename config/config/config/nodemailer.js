b2c9b7554462cbb8b4ee4d5c056bb6592d361073
Abbaty1255 <110620280+Abbaty1255@users.noreply.github.com>
2026-07-27 14:40:12 +0100

    Update nodemailer.js

--- config/config/config/nodemailer.js
+++ config/config/config/nodemailer.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_PORT == 465,
+  port: Number(process.env.SMTP_PORT),
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
@@ -13,14 +13,18 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_FROM,
+    const info = await transporter.sendMail({
+      from: `"AutoNest Drive" <${process.env.EMAIL_FROM}>`,
       to,
       subject,
       html
     });
+
+    console.log('Email sent:', info.messageId);
+
   } catch (error) {
-    console.error('Nodemailer Error:', error.message);
+    console.error('Nodemailer Error:', error);
+    throw error;
   }
 };
 
